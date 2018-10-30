0c50c50e40f8c2cdaae18f70f07f11271cc080ba
EaglesWatch <2002zdn@gmail.com>
2018-10-30 13:13:03 -0700

    added a spinning block that'll late become a power up or something

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -7,31 +7,53 @@ public class PlayerController : MonoBehaviour {
     public float speed = 10.0F;
     public float max_speed = 12.0F;
     public float jump_height = 75.0F;
-
+    private Collider m_collider;
+    private float collider_radius = 0.0F;
+    private float grounded_epsilon = 0.5F;
+    public int user_layer_platform;
+    private float get_axis_horizontal = 0.0F;
+    private bool get_key_down_space = false;
+    public string pickup_tag;
 
     // Use this for initialization
     void Start() {
         m_rb = GetComponent<Rigidbody>();
+        m_collider = GetComponent<Collider>();
+        collider_radius = m_collider.bounds.extents.y;
     }
 
     // Update is called once per frame
     void Update() {
+        get_axis_horizontal = Input.GetAxis("Horizontal");
+        get_key_down_space = Input.GetKey(KeyCode.Space);
 
     }
     private void FixedUpdate()
     {
-        float movement = Input.GetAxis("Horizontal");
+        float movement = get_axis_horizontal;
         m_rb.AddForce(new Vector3(movement * speed, 0.0F, 0.0F));
         m_rb.velocity = new Vector3(
             Mathf.Clamp(m_rb.velocity.x, -max_speed, max_speed),
             m_rb.velocity.y, m_rb.velocity.z
             );
         //jumping mechanic
-        if (Input.GetKeyDown(KeyCode.Space))
-            m_rb.AddForce(0.0F, jump_height, 0.0F);    
-        
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded())
+            m_rb.AddForce(0.0F, jump_height, 0.0F);
+
     }
-
-
+    bool isGrounded()
+    {
+        int platform_layer = 1 << user_layer_platform;
+        return Physics.Raycast(
+            transform.position,
+            Vector3.down,
+            collider_radius + grounded_epsilon,
+            platform_layer);
+    }
+    private void OnTriggerEnter(Collider other)    
+    {
+        if (other.gameObject.CompareTag(pickup_tag))
+            other.gameObject.SetActive(false);
+        }
 
 }
